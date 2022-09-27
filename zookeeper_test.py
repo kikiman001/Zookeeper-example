9164b67ea82153760ca20b493b208af7978997a5
enrique <enriquepriego505@gmail.com>
2022-09-27 18:34:39 -0500

    Update zookeeper_test.py

--- zookeeper_test.py
+++ zookeeper_test.py
@@ -5,14 +5,24 @@ class TestZookeeper(unittest.TestCase):
 
     def test_crear_znode(self):
         tree = Ztree()
-        tree.create('/node1', 'algo', True, True, 10, '/')
-        self.assertEqual(tree.getData('/node1'), 'algo')
-
-    def test_no_se_puede_crear(self):
-        with self.assertRaises(Exception):
-            tree = Ztree()
-            tree.create('/node1/node2/node3', 'algo', True, True, 10, None)
-
+        tree.create('/n1', '1', True, True, 10, '/')
+        self.assertEqual(tree.getData('/n1'), '1')
+    def test_crear_znode_2(self):
+        tree = Ztree()
+        tree.create('/n2', '2', True, True, 10, '/')
+        self.assertEqual(tree.getData('/n2'), '2')
+    def test_crear_znode_3(self):
+        tree = Ztree()
+        tree.create('/n2/n3', '2', True, True, 10, '/')
+        self.assertEqual(tree.getData('/n2/n3'), '2')
+    def test_borrar(self):
+        tree = Ztree()
+        tree.delete('/n2/n3',1)
+        self.assertFalse(tree.getData('/n2/n3'),0)
+    def test_show(self):
+        tree = Ztree()
+        tree.showTree()
+        
 
 if __name__ == '__main__':
     unittest.main()
